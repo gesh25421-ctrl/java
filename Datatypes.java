b74cc620920775f05282670792bbc822b8efe713
gesh25421-ctrl <gesh25421@gmail.com>
2026-09-21 14:28:44 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Datatypes{
+	static byte a;
+	static short b;
+	static int c;
+	static float e;
+	static double f;
+	static long g;
+	static char h;
+	static boolean i;
+	public static void main(String args[]){
+			System.out.println("byte:"+a+"\nshort:"+b+"\nint:"+c+"\nfloat:"+e+"\ndouble:"+f+"\nlong:"+g+"\nchar:"+h+"\nboolean:"+i);
+	}
+	}
